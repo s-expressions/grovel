ba41c7c79fb0da62879bc1495f6b743215331b47
Lassi Kortela <lassi@lassi.io>
2020-09-11 11:00:47 +0300

    Write Gambit errors to stderr, not stdout

--- errno.sh
+++ errno.sh
@@ -2,6 +2,6 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 set -x
-gsi-script . errno.scm > errno.c
+gsi-script -:debug=+ . errno.scm >errno.c
 cc -Wall -Wextra -Wno-unused-function -ansi -pedantic -o errno errno.c
 ./errno | tee errno.out
--- example.sh
+++ example.sh
@@ -2,6 +2,6 @@
 set -eu -o pipefail
 cd "$(dirname "$0")"
 set -x
-gsi-script . example.scm > example.c
+gsi-script -:debug=+ . example.scm >example.c
 cc -Wall -Wextra -Wno-unused-function -ansi -pedantic -o example example.c
 ./example | tee example.out
